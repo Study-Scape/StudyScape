fd28a0b43862764bef9d9b76e6ee7bb7fb34defa
Brandon Chee <bchee878@gmail.com>
2025-02-15 13:21:45 -0800

    put map component in a div

--- study-scape/app/page.tsx
+++ study-scape/app/page.tsx
@@ -2,8 +2,10 @@ import MapboxComponent from '@/components/MapboxComponent';
 
 export default function Home() {
   return (
-    <main style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '81vh', width: '81vw' }} suppressHydrationWarning={true}>
-      <MapboxComponent />
+    <main>
+      <div style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '81vh', width: '81vw' }} suppressHydrationWarning={true}>
+        <MapboxComponent />
+      </div>
     </main>
   );
 }
