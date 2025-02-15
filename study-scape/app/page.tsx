636faf0d8a62c1ed65fe50f74fa472fe9423e6f8
Brandon Chee <bchee878@gmail.com>
2025-02-15 13:17:29 -0800

    changed map alignment in window

--- study-scape/app/page.tsx
+++ study-scape/app/page.tsx
@@ -2,7 +2,7 @@ import MapboxComponent from '@/components/MapboxComponent';
 
 export default function Home() {
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', width: '100vh' }}>
+    <main style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '81vh', width: '81vw' }} suppressHydrationWarning={true}>
       <MapboxComponent />
     </main>
   );
