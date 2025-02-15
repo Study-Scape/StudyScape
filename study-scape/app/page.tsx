1958a1c2c49ab38faca61e1e7c58b9a3ab0b3d0c
Brandon Chee <bchee878@gmail.com>
2025-02-15 11:26:55 -0800

    added map feature

--- study-scape/app/page.tsx
+++ study-scape/app/page.tsx
@@ -1,16 +1,9 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import MapboxComponent from '@/components/MapboxComponent';
 
-export default async function Home() {
+export default function Home() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', width: '100vh' }}>
+      <MapboxComponent />
+    </main>
   );
 }
