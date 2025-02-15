e629898cd000478a600c99908760b10ffed5d6da
Brandon Chee <bchee878@gmail.com>
2025-02-15 13:49:26 -0800

    changed height of map

--- study-scape/app/page.tsx
+++ study-scape/app/page.tsx
@@ -3,7 +3,7 @@ import MapboxComponent from '@/components/MapboxComponent';
 export default function Home() {
   return (
     <main>
-      <div style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '81vh', width: '81vw' }} suppressHydrationWarning={true}>
+      <div style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '91.4vh', width: '81vw' }} suppressHydrationWarning={true}>
         <MapboxComponent />
       </div>
     </main>
