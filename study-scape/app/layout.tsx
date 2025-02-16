a384aa44fa2696238834c1869b70d669ab5a2562
Carl Pontino <108214113+carlpont@users.noreply.github.com>
2025-02-16 02:22:50 -0800

    Get all locations from db for Locations page and Add 'Show All Locations' button

--- study-scape/app/layout.tsx
+++ study-scape/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "StudyScape",
+  description: "Empower University of Washington students to make informed decisions about where to study based on their preferences and needs",
 };
 
 const geistSans = Geist({
--- study-scape/app/page.tsx
+++ study-scape/app/page.tsx
@@ -1,10 +1,30 @@
 import MapboxComponent from '@/components/MapboxComponent';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <div style={{ position: 'absolute', right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '91.4vh', width: '81vw' }} suppressHydrationWarning={true}>
+      <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+        <div 
+          style={{
+          position: "absolute",
+          height: "100%",
+          width: "100%",
+          }}
+        >
         <MapboxComponent />
+      </div>
+      <Button
+        style={{
+        position: "absolute",
+        top: "50px",
+        left: "50px",
+        }}
+        asChild
+      >
+        <Link href="./locations">Show All Locations</Link>
+      </Button>
       </div>
     </main>
   );
