c4b89b2590b117e8c7a2c61b5b8939eacd0ee95d
Sanjana Singh <sanjana6@cs.washington.edu>
2025-02-18 11:02:27 -0800

    Navigation Bar

--- study-scape/app/layout.tsx
+++ study-scape/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -24,6 +26,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+      <nav className="w-full flex justify-between items-center px-6 border-b border-b-foreground/10 h-16 bg-black text-white">
+            {/* Logo */}
+            <div className="text-lg font-bold text-purple">
+              <Link href="/">StudyScape</Link>
+            </div>
+            {/* Navigation Links */}
+            <div className="flex gap-10">
+              <Link href="/" className="hover:text-purple-500">
+                Home
+              </Link>
+              <Link href="/About" className="hover:text-purple-500">
+                About
+              </Link>
+              <Link href="/Bookmarks" className="hover:text-purple-500">
+                Bookmarks
+              </Link>
+              <Link href="/Profile" className="hover:text-purple-500">
+                Profile
+              </Link>
+            </div>
+
+          </nav>
         <main>{children}</main>
       </body>
     </html>
