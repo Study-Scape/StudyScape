3879c7be1332f7dfbf72d1133488a97804dd2c09
Dawn Mai <dawnmai@cs.washington.edu>
2025-03-09 14:33:36 -0700

    updated amenities

--- study-scape/components/realtime-locations-sheet.tsx
+++ study-scape/components/realtime-locations-sheet.tsx
@@ -158,12 +158,12 @@ export default function AllLocations({ serverLocations }: {serverLocations: any}
                     <div className='flex flex-wrap gap-1'>
                         {location.hasWifi && (
                           <span className="px-2 py-1 bg-blue-100 text-blue-800 text-xs rounded-full">
-                            WiFi
+                            WiFi Available
                           </span>
                         )}
                         {location.hasFood && (
                           <span className="px-2 py-1 bg-green-100 text-green-800 text-xs rounded-full">
-                            Food
+                            Food Nearby
                           </span>
                         )}
                         {location.hasRestrooms && (
@@ -173,7 +173,7 @@ export default function AllLocations({ serverLocations }: {serverLocations: any}
                         )}
                         {location.hasPrinters && (
                           <span className="px-2 py-1 bg-yellow-100 text-yellow-800 text-xs rounded-full">
-                            Printer
+                            Printers Available
                           </span>
                         )}
                     </div>
@@ -184,10 +184,9 @@ export default function AllLocations({ serverLocations }: {serverLocations: any}
                         <small><span className='text-red-600 text-xs pr-4'> stars</span></small>
                         <small><span className='text-green-600 text-xs'>Noise level: </span></small>
                         <span className='text-green-600 rounded-full'>
-                          
-
-                          {location.soundLevel}
-                          
+                          {location.soundLevel == 1 && (<span className='text-green-600 rounded-full'> Quiet </span>)}
+                          {location.soundLevel == 2 && (<span className='text-green-600 rounded-full'> Chatter </span>)}
+                          {location.soundLevel == 3 && (<span className='text-green-600 rounded-full'> Loud </span>)}
                           </span>
                       </div>
 
@@ -200,17 +199,17 @@ export default function AllLocations({ serverLocations }: {serverLocations: any}
                         <div className='flex flex-wrap gap-1'>
                             {location.hasElevator && (
                               <span className='px-2 py-1 bg-orange-100 text-orange-800 text-xs rounded-full'>
-                                {location.hasElevator}
+                                Elevator Available
                               </span>
                             )}
                             {location.quietSpaces && (
                               <span className='px-3 py-1 bg-blue-100 text-blue-800 text-xs rounded-full'>
-                                  {location.quietSpaces}
+                                  Quiet Spaces
                               </span>
                             )}
                             {location.hasBikeRack && (
                               <span className='px-3 py-1 bg-blue-100 text-blue-800 text-xs rounded-full'>
-                                  {location.hasBikeRack}
+                                  Bike Rack Available
                               </span>
                             )}
                           </div>
