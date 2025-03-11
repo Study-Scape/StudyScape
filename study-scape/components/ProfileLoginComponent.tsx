e92d8d6d096d9d00121494a1d94cd0167ef7bdb6
musicalPop <144726333+musicalPop@users.noreply.github.com>
2025-03-11 08:20:51 +0000

    login button for profile page

---
+++ study-scape/components/ProfileLoginComponent.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function ProfileLoginComponent() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Button 
+        onClick={() => router.push('/login')}
+        className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+      >
+        Please login to make your profile!
+      </Button>
+    </div>
+  );
+}
